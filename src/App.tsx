7e977a541d3a294836d5091dff1824bc098a7291
Sanjeev Midda <s.midda1@gmail.com>
2026-08-18 20:52:47 +0100

    feat: implement event management functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useMemo, useState } from "react";
 import "./index.css";
 
+import { Event, initialEvents } from "./data/events";
+
+import EventForm from "./components/EventForm";
+import EventList from "./components/EventList";
+
+import { getUpcomingEventCount, sortEventsByDate } from "./utils/eventUtils";
+
 function App() {
-  return <div className="App"></div>;
+  const [events, setEvents] = useState<Event[]>(initialEvents);
+
+  return (
+    <main className="app">
+      <h1>Event Management</h1>
+
+      <p>
+        Upcoming events: <strong>{upcomingEventCount}</strong>
+      </p>
+
+      <EventForm
+        eventToEdit={eventToEdit}
+        onSave={handleSaveEvent}
+        onCancelEdit={handleCancelEdit}
+      />
+
+      <EventList
+        events={sortedEvents}
+        onEdit={handleEditEvent}
+        onDelete={handleDeleteEvent}
+      />
+    </main>
+  );
 }
 
 export default App;
