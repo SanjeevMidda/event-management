39444bc3bff626fdde63e0f99e7c8ed842f629bc
Sanjeev Midda <s.midda1@gmail.com>
2026-08-18 22:40:43 +0100

    refactor: remove unused folders

--- src/components/EventForm.test.tsx
+++ src/components/EventForm.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import EventForm from "../EventForm";
+import EventForm from "./EventForm";
 
 describe("EventForm", () => {
   const setup = () => {
